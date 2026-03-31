53ac4ae7f2418484bbb6838ddbcc415450c051dd
BCO-Taylor <taylorh@businessco.com.au>
2026-04-01 09:58:40 +1100

    m

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,10 @@
-async function getPosts() {
-  const res = await fetch(process.env.NEXT_PUBLIC_GRAPHQL_URL as string, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      query: `{
-        posts {
-          nodes {
-            title
-            slug
-          }
-        }
-      }`
-    })
-  });
-  const { data } = await res.json();
-  return data.posts.nodes;
-}
+
 
 export default async function Home() {
-  const posts = await getPosts();
   return (
     <main>
       <h1>Posts</h1>
-      {posts.map((post: { title: string, slug: string }) => (
-        <p key={post.slug}>{post.title}</p>
-      ))}
+      
     </main>
   );
 }
